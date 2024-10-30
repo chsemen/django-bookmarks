61cf7a56ea2ad7c130b8251432a11c3553f43044
Semen <ch-semen@rambler.ru>
2024-10-30 19:37:37 +0300

    chapter 6 end

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'social_django',    
+    'social_django',  
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
