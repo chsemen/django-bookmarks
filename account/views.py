bcde71176c47d95829d27c6bdf97895918dcd935
Semen <ch-semen@rambler.ru>
2024-11-02 14:19:42 +0300

    ch7 p299

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import authenticate, get_user_model, login
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404, render
 from .forms import (
     LoginForm, 
     UserRegistrationForm,
@@ -9,7 +10,7 @@ from .forms import (
 )
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Profile
+from .models import Contact, Profile
 
 # Create your views here.
 
@@ -100,3 +101,45 @@ def edit(request):
         }
     )
 
+User = get_user_model()
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active = True)
+    return render(
+        request,
+        'account/user/list.html',
+        {'section': 'people', 'users': users}
+    )
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username = username, is_active = True)
+    return render(
+        request,
+        'account/user/detail.html',
+        {'section': 'people', 'user': user}
+    )
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from = request.user,
+                    user_to = user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user
+                ).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
